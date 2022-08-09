5ac52b7f2c3208b41b2690ac41be62b6d69d3b01
shi jia <shijia2118@126.com>
2022-08-09 10:25:19 +0800

    ios端使用no-uuid

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/mo/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/mo/Code/Flutter/tobias/example"
+export "FLUTTER_ROOT=/Users/hanqingxiu/fvm/versions/2.10.5"
+export "FLUTTER_APPLICATION_PATH=/Users/hanqingxiu/project/tobias/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
